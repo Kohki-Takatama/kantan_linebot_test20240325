b4ac2a7e956d3b489d3580c6e1a1c9d4e62b2ec6
Kohki Takatama <kohkitakatama@TamasMacbook.local>
2024-03-26 07:19:05 +0900

    add: narou_api

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -11,9 +11,10 @@ class LinebotController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           message.push(parroting(event))
           message.push(runtequn_image)
-          message.push(narou_api)
+          message.push(narou_api_ad)
         end
       end
+      qiita_trend_api(event)
       client.reply_message(event['replyToken'], message)
     end
   end
@@ -29,32 +30,66 @@ class LinebotController < ApplicationController
     {type: 'image', originalContentUrl: runtequn, previewImageUrl: runtequn}
   end
 
-  def narou_api
+  def narou_api_ad
     uri = URI('https://api.syosetu.com/novelapi/api/')
     params = {
       out: 'json',
-      ncode: 'n8920ex'
+      ncode: 'n8920ex',
     }
     uri.query = URI.encode_www_form(params)
 
     res = Net::HTTP.get_response(uri)
     novel = JSON.parse(res.body)
-    {type: 'text', text: "#{novel[1]['title']}\n\n#{novel[1]['story']}"}
+    p novel
+    {type: 'text', text: "#{novel[1]['title']}\n\n#{novel[1]['story']}\n\n最終更新：#{novel[1]['general_lastup']}"}
   end
 
   def runteq_quiz_hisaju
+
   end
 
   def runteq_quiz_runteq
   end
 
   def confirm_message
+    {
+      "type": "template",
+      "altText": "this is a confirm template",
+      "template": {
+        "type": "confirm",
+        "text": "Are you sure?",
+        "actions": [
+        {
+          "type": "message",
+          "label": "Yes",
+          "text": "yes"
+        },
+        {
+          "type": "message",
+          "label": "No",
+          "text": "no"
+        }
+      ]
+    }
+  }
   end
 
   def image_carousel
   end
 
-  def qiita_trend_api
+  def qiita_trend_api(event)
+    uri = URI('https://qiita-api.vercel.app/api/trend')
+    res = Net::HTTP.get_response(uri)
+    articles = JSON.parse(res.body)
+    message = []
+    # トレンド上位5記事のみ抽出
+    5.times {|i|
+      hash = {}
+      hash[:type] = "text"
+      hash[:text] = response[i]["node"]["linkUrl"]
+      message.push(hash)
+    }
+    client.reply_message(event['replyToken'],  message)
   end
 
   def client
